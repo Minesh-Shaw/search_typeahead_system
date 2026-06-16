ec340149464a06f52f21317cfe3b16712aaba3af
MineshShaw <mineshshaw2007@gmail.com>
2026-06-16 11:38:31 +0530

    Initial commit

---
+++ src/main/java/com/minesh/search_typeahead_system/SearchTypeaheadSystemApplication.java
@@ -0,0 +1,13 @@
+package com.minesh.search_typeahead_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchTypeaheadSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchTypeaheadSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minesh/search_typeahead_system/SearchTypeaheadSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minesh.search_typeahead_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchTypeaheadSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
